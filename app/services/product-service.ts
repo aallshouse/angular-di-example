5ce7baa93444dd30bcf29221b513fc709c8c8e45
Andrew Allshouse <allshouseandrew@aol.com>
2017-08-24 18:06:26 -0400

    initial commit

---
+++ app/services/product-service.ts
@@ -0,0 +1,13 @@
+export class Product {
+    constructor(
+        public id: number,
+        public title: string,
+        public price: number,
+        public description: string) {}
+}
+
+export class ProductService {
+    getProduct(): Product {
+        return new Product(0, 'iPhone', 249.99, 'The latest iPhone, 7-inch screen');
+    }
+}
